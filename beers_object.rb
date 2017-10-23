c77004dd7480694bbc615960862ed152aad8d09e
sbaxter95 <sbaxter95@outlook.com>
2017-10-23 14:59:51 +0100

    got a service for single beer

--- beers_object.rb
+++ beers_object.rb
@@ -6,4 +6,23 @@ class BeerServices
 
   base_uri 'https://api.punkapi.com/v2/'
 
+  def single_beer_service(beer_id)
+    JSON.parse(self.class.get("/beers/#{beer_id}").body)
+  end
+
+  def all_beers_service
+
+  end
+
+  def random_beer_service
+
+  end
+
+  def query_beers_service
+
+  end
+
 end
+
+beer = BeerServices.new
+p beer.single_beer_service('5')
