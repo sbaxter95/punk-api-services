5b03f1f6b6d96b4db3aebe13b2f6be5461a7ec36
sbaxter95 <sbaxter95@outlook.com>
2017-10-23 16:40:27 +0100

    made service for search parameters

--- beers_object.rb
+++ beers_object.rb
@@ -10,6 +10,7 @@ class BeerServices
     @params_array = %w(abv_gt abv_lt ibu_gt ibu_lt ebc_gt ebc_lt beer_name yeast brewed_before brewed_after hops malt food ids)
     @k_array = []
     @v_array = []
+    @url = ""
   end
 
   def single_beer_service(beer_id)
@@ -24,7 +25,7 @@ class BeerServices
 
   end
 
-  def query_beers_service(hash)
+  def check_for_params(hash)
     hash.each do |k,v|
       if @params_array.include?(k)
         @k_array.push(k)
@@ -33,12 +34,31 @@ class BeerServices
         raise 'No matches'
       end
     end
-    p @k_array
-    p @v_array
+    @k_array
+    @v_array
   end
 
+  def add_params_to_url
+    if @k_array.length > 0 && @v_array.length > 0
+      if @k_array.length == 1 && @v_array.length == 1
+        @url << "?#{@k_array[0]}=#{@v_array[0]}"
+      else
+        @url << "?#{@k_array[0]}=#{@v_array[0]}"
+        for i in 1..(@k_array.length)-1 do
+          @url << "&#{@k_array[i]}=#{@v_array[i]}"
+        end
+      end
+    end
+  end
+
+  def search_beers
+    JSON.parse(self.class.get("/beers#{@url}").body)
+  end
+  
 end
 
 beer = BeerServices.new
 # p beer.single_beer_service('5')
-beer.query_beers_service('yeast' => 'test', 'ids' => '2')
+beer.check_for_params({'yeast' => 'Wyeast_1056_-_American_Ale', 'abv_gt' => 8})
+beer.add_params_to_url
+p beer.search_beers
