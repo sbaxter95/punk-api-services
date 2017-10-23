abb60ccdb5935ad9282dce51207e177e22954de3
sbaxter95 <sbaxter95@outlook.com>
2017-10-23 15:54:42 +0100

    can push values of the key value pairs into arrays

--- beers_object.rb
+++ beers_object.rb
@@ -8,6 +8,8 @@ class BeerServices
 
   def initialize
     @params_array = %w(abv_gt abv_lt ibu_gt ibu_lt ebc_gt ebc_lt beer_name yeast brewed_before brewed_after hops malt food ids)
+    @k_array = []
+    @v_array = []
   end
 
   def single_beer_service(beer_id)
@@ -25,15 +27,18 @@ class BeerServices
   def query_beers_service(hash)
     hash.each do |k,v|
       if @params_array.include?(k)
-        puts v
+        @k_array.push(k)
+        @v_array.push(v)
       else
         raise 'No matches'
       end
     end
+    p @k_array
+    p @v_array
   end
 
 end
 
 beer = BeerServices.new
 # p beer.single_beer_service('5')
-p beer.query_beers_service('bob' => 'test')
+beer.query_beers_service('yeast' => 'test', 'ids' => '2')
