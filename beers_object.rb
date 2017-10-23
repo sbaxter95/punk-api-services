34e8e143d12540a4ffd5880229f4b0a1276bf499
sbaxter95 <sbaxter95@outlook.com>
2017-10-23 15:43:05 +0100

    Can loop through inputted hash and get matched values to print to screen

--- beers_object.rb
+++ beers_object.rb
@@ -6,6 +6,10 @@ class BeerServices
 
   base_uri 'https://api.punkapi.com/v2/'
 
+  def initialize
+    @params_array = %w(abv_gt abv_lt ibu_gt ibu_lt ebc_gt ebc_lt beer_name yeast brewed_before brewed_after hops malt food ids)
+  end
+
   def single_beer_service(beer_id)
     JSON.parse(self.class.get("/beers/#{beer_id}").body)
   end
@@ -18,11 +22,16 @@ class BeerServices
 
   end
 
-  def query_beers_service
-
+  def query_beers_service(hash)
+    hash.each do |k,v|
+      if @params_array.include?(k)
+        puts v
+      end
+    end
   end
 
 end
 
 beer = BeerServices.new
-p beer.single_beer_service('5')
+# p beer.single_beer_service('5')
+p beer.query_beers_service('yeast' => 'test')
